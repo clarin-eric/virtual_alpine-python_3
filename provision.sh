619e8286bbe6b17131043d94ec89a1201e185bdc
Sander M <S.N.Maijers@gmail.com>
2015-08-28 15:15:12 +0200

    * Make sure apk cache gets purged afterwards.

--- provision.sh
+++ provision.sh
@@ -4,4 +4,5 @@ apk --verbose --progress update --purge # --update-cache
 apk --verbose --progress upgrade --latest # --update-cache --available
 apk --verbose --progress add --upgrade 'python3=3.4.3-r2' 'ca-certificates'
 pip3 --no-cache-dir --isolated install --upgrade 'setuptools' 'pip'
+apk cache --purge clean
 # apk --verbose cache sync
